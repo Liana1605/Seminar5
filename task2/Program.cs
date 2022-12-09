b61871cca84480cec6387781ca386cc9da040fd2
liana16 <liana_you@bk.ru>
2022-12-09 20:27:02 +0300

    Задача 2

---
+++ task2/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] names = {"Bob", "Ann", "Liz", "Mike"};
+int[] ages = {10, 17, 15, 21};
+string s10 = string.Join(" ", names);
+Console.WriteLine(s10);
+string s11 = string.Join("---", ages);
+Console.WriteLine(s11);
