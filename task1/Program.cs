8a652c5d906a9be2352fa64a66041cc67f2d8601
liana16 <liana_you@bk.ru>
2022-12-09 20:26:02 +0300

    Задача 1

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿string s1 = "Bob";
+string s2 = "Anna";
+string s3 = s1 + " - " + s2;
+Console.WriteLine(s3);
+
+int num = 123;
+string sNum = Convert.ToString(num);
+
+int s3Len = s3.Length;
+Console.WriteLine(s3Len);
