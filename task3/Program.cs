acae6d02a402680ac418a15b0da8c4e335830d00
liana16 <liana_you@bk.ru>
2022-12-09 20:28:48 +0300

    Задача 3

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿string s = ("1 2 3 4 5 6 7");
+string[] nums = s.Split(' ');
+foreach (var num in nums)
+{
+    Console.WriteLine($"Значение: {num}");
+}
+int[] n = nums.Select(Int32.Parse).ToArray();
+foreach (var i in n)
+{
+    Console.Write($"{i} ");
+}
