09f2a1e87acbb087de98fe373b8b288b1a8ff0dd
liana16 <liana_you@bk.ru>
2022-12-09 20:30:34 +0300

    Задача 32

---
+++ task32/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+//[-4,-8,8,2] -> [4,8,-8-2]
+
+int[] arrRand(int len) // функция заполнения массива произв длины случайными числами
+{
+    int[] a = new int [len];
+    for (int i=0; i < a.Length; i++)
+    {
+        a[i] = new Random().Next(-9, 10);
+    }
+    return a;
+}
+
+void arrPrint(int[] a) // функция вывода массива
+{
+    for (int i = 0; i < a.Length; i++)
+        Console.Write($"{a[i]} ");
+    Console.WriteLine();
+}
+
+int [] arr = arrRand(4);
+arrPrint(arr);
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = arr[i] * (-1);
+}
+arrPrint(arr);
